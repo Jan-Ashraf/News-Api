108504fc1d8713b6711748556591df5e7dcd1439
Jan Ashraf <jan.ashraf756@gmail.com>
2026-04-19 16:03:49 +0200

    Main

--- README.md
+++
@@ -1,3 +0,0 @@
-# api_session
-
-A new Flutter project.
--- android/app/src/main/kotlin/com/example/api_session/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.api_session
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
